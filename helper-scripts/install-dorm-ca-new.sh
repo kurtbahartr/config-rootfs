37bd446ed19789c450c91cd9f756dda9068a5fd1
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-11-15 15:37:43 +0300

    Update the script for dorm WiFi and save it as a new file
    The command `update-ca-certificates` has been replaced with `trust`
    along with path changes. The old one is kept for compatibility with
    Debian and its downstream for now.

---
+++ helper-scripts/install-dorm-ca-new.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Quick script to install the CA certificate for dormitory WiFi if ever needed.
+
+# Download the CA to its intended directory
+sudo wget -O /etc/ca-certificates/trust-source/anchor/dorm-ca.crt https://github.com/kurtbahartr/config-rootfs/raw/master/common/usr/local/share/ca-certificates/dorm-ca.crt
+sudo trust extract-compat
