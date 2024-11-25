9a38b12ec698f0fd039bbd0c1cdb9f34c2f94fdc
Bahar KURT <kurtbahartr@users.noreply.github.com>
2024-11-25 11:32:44 +0300

    install-dorm-ca-new: Fix the path for trust anchors

--- helper-scripts/install-dorm-ca-new.sh
+++ helper-scripts/install-dorm-ca-new.sh
@@ -3,5 +3,5 @@
 # Quick script to install the CA certificate for dormitory WiFi if ever needed.
 
 # Download the CA to its intended directory
-sudo wget -O /etc/ca-certificates/trust-source/anchor/dorm-ca.crt https://github.com/kurtbahartr/config-rootfs/raw/master/common/usr/local/share/ca-certificates/dorm-ca.crt
+sudo wget -O /etc/ca-certificates/trust-source/anchors/dorm-ca.crt https://github.com/kurtbahartr/config-rootfs/raw/master/common/usr/local/share/ca-certificates/dorm-ca.crt
 sudo trust extract-compat
